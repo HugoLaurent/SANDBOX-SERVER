35d4411f571e28fa7c93d051076a16e87d7791d2
HugoLaurent <lauhugopro@gmail.com>
2024-09-20 10:07:09 +0200

    Refactor paymentController to log request parameters and headers

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -2,7 +2,17 @@ const payment = require("../json/payfip.js");
 
 const notification = (req, res) => {
   console.log("Je reçois la notification");
-  console.log(req);
+
+  // Afficher les paramètres de la requête (query, params)
+  console.log("Query params:", req.query);
+  console.log("Route params:", req.params);
+
+  // Vérifier et afficher les headers
+  console.log("Headers:", req.headers);
+
+  // Vérifier et afficher le body de la requête
+  console.log("Body:", req.body);
+
   res.send("Notification reçue");
 };
 
@@ -12,15 +22,6 @@ const redirection = (req, res) => {
   // Récupérer les paramètres de requête (req.query) côté serveur
   const params = {
     idop: req.query.idop,
-    NUMCLI: req.query.NUMCLI,
-    EXER: req.query.EXER,
-    REFDET: req.query.REFDET,
-    OBJET: req.query.OBJET,
-    MONTANT: req.query.MONTANT,
-    MEL: req.query.MEL,
-    URLNOTIF: req.query.URLNOTIF,
-    URLREDIRECT: req.query.URLREDIRECT,
-    SAISIE: req.query.SAISIE,
   };
 
   console.log(params);
@@ -32,19 +33,7 @@ const redirection = (req, res) => {
         <h1>Redirection reçue avec les paramètres suivants :</h1>
         <ul>
           <li><strong>ID Operation (idop):</strong> ${params.idop}</li>
-          <li><strong>Numéro Client (NUMCLI):</strong> ${params.NUMCLI}</li>
-          <li><strong>Exercice (EXER):</strong> ${params.EXER}</li>
-          <li><strong>Référence Détail (REFDET):</strong> ${params.REFDET}</li>
-          <li><strong>Objet (OBJET):</strong> ${params.OBJET}</li>
-          <li><strong>Montant (MONTANT):</strong> ${params.MONTANT}</li>
-          <li><strong>Email (MEL):</strong> ${params.MEL}</li>
-          <li><strong>URL Notification (URLNOTIF):</strong> ${
-            params.URLNOTIF
-          }</li>
-          <li><strong>URL Redirection (URLREDIRECT):</strong> ${
-            params.URLREDIRECT
-          }</li>
-          <li><strong>Saisie (SAISIE):</strong> ${params.SAISIE}</li>
+          
         </ul>
         <pre>${JSON.stringify(params, null, 2)}</pre>
       </body>
